6bc6b80cd4d0b7bd2075bb1c9d14e7ba25d89fab
Lee Paludan <leepaludan@gmail.com>
2018-09-24 15:02:56 +0200

    Project setup

---
+++ build/CMakeFiles/ProgramAnalysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgramAnalysis.dir/main.cpp.o"
+  "ProgramAnalysis.pdb"
+  "ProgramAnalysis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgramAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
