ba02617409ea2d90dde3653d2076d42d6492f5eb
Lee Paludan <Leepaludan@gmail.com>
2018-11-29 22:31:19 +0100

    Close to complete, exceptions occur that need to be hanled

--- Analysis.fsx
+++ Analysis.fsx
@@ -1,9 +1,11 @@
 namespace ProgramAnalysis
-open System.Collections.Generic
+open System
 #load "Types.fsx"
 #load "Programgraph.fsx"
 
 open ProgramAnalysis.Types
+open Programgraph
+
 module Analysis =
 
     let labelString = function
@@ -16,7 +18,26 @@ module Analysis =
         We can ignore warnings here, as the only actions ever to be added as input 
         here will be atomic actions 
     *)
-    let rec grabIdentifier (act : Action) = 
+
+    let mapIsSubset (m1 : Map<'a, 'b list>) (m2 : Map<'a, 'b list>) : bool =
+        m1
+        |> Map.fold (fun result key value ->
+            result &&
+            match m2.TryFind key with
+            | Some y -> Set.isSubset (Set.ofList value) (Set.ofList y)
+            | _ -> false
+        ) true
+    let mapValueUnion (m1 : Map<'a, 'b list>) (m2 : Map<'a, 'b list>) =
+        m1 |> Map.fold (fun m key value ->
+            match Map.tryFind key m with
+            | Some x -> m.Add(key, value @ x)
+            | None   -> m
+        ) m2
+        
+    let leastUpperBound (m1 : Map<'a, 'b list>) (m2 : Map<'a, 'b list>) f =
+        m1 |> f <| m2
+
+    let grabIdentifier (act : Action) = 
         match act with
         | S(s) -> match s with
                   | Ass (l, _)    -> labelString l
@@ -29,19 +50,35 @@ module Analysis =
                   | DArray (_, s) -> s     
         | B(_) -> "Ø"
 
-    let rec tfRD ( ( ( (q :int), ( (q' : int), (act : Action) ) ), (m : Map<string, (int * int) list>)) ) : Map<string, (int * int) list> =
-         m.Add(grabIdentifier act, [(q, q')])
+    let analysisResultInit (pg : ProgramGraph) bottom : Map<int, Map<'a, 'b>> =
+        pg |> Map.fold (fun x key _ ->
+            x.Add(key, bottom)
+        ) Map.empty
 
-    let workListAlgorithm (pg : ProgramGraph) : Map<string, (int * int) list> = 
-        pg |> Map.fold (fun w key value -> 
-                value |> 
-                List.fold (fun x ( (q, a) : (int * Action) ) ->
-                    match a with
-                    | B _ -> x
-                    | _ -> List.append <| x <| [(key, (q, a))]
-                ) w 
-              ) []
-        |> 
-        List.fold (fun x e ->
-             Programgraph.mapMerge <| x <|tfRD(e, x)
-        ) Map.empty 
+    let workListInit (pg : ProgramGraph) : int list =
+        pg |> Map.fold(fun x key _ ->
+                x @ [key]
+            ) []
+            
+    let tfRD (q : int) (q' : int, act : Action) (m : Map<string, (int * int) list>)  : Map<string, (int * int) list> =
+         m.Add(grabIdentifier act, [(q, q')]) 
+
+    let rec workListSolve (pg : ProgramGraph) workList (analysisResult : Map<int, Map<string, 'b list>>) (tf : int -> int  * Action -> Map<string,(int * int) list> -> Map<string,(int * int) list> ) = 
+        match workList with
+        | [] -> analysisResult
+        | q::xs ->
+            let (analysisResult', workList') =
+                Map.find q pg
+                |> List.fold (fun ((m : Map<_,_>), workList) (q', act) -> 
+                    if ( not ( mapIsSubset (tf q (q', act) (Map.find q m)) (Map.find q' analysisResult) ) ) 
+                        then m.Add(q', (leastUpperBound (Map.find q' analysisResult) (tf q (q', act) (Map.find q m) ) mapValueUnion) ), xs @ [q'] 
+                    else (m, xs) 
+                ) (analysisResult, workList)
+            workListSolve pg workList' analysisResult' tf
+        
+        
+    let workListAlgorithm (pg : ProgramGraph) bottom dhat tf =
+        let mutable analysisResult = analysisResultInit <| pg <| bottom
+        let mutable workList = workListInit pg
+        //analysisResult.Add(0, [dhat])
+        workListSolve pg workList analysisResult tf
--- Main.fsx
+++ Main.fsx
@@ -9,7 +9,7 @@ open ProgramAnalysis.Analysis
 let fac = (DD(DInt("x"), DInt("y")), Seq(Ass(Id "y", Int 1), 
               While(Neg(ROp(Var (Id "y"), Eq, Var (Id "x"))), 
                     Seq(Ass(Id "y", AOp(Var (Id "x"), Mul, Var (Id "y"))) , Ass(Id "x", AOp(Var (Id "x"), Sub, Int 1)) ))
-             ));;
+             ))
 
 let ifthen = (DD(DInt("x"), DInt("y")), IT(ROp(Var(Id "x"), Eq, Int 0), Ass(Id "x", Int 1)))
 
@@ -18,4 +18,11 @@ let sg = stmGraph (amountOfDecls (fst fac)) -1 (snd fac) dg
 
 let pg = mapMerge dg sg
 
-let b = workListAlgorithm pg
+//let a = workListInit pg
+//let b = analysisResultInit <| pg <| Map.empty
+
+
+let analysisResult = analysisResultInit pg Map.empty
+let workList = workListInit pg
+let s = workListSolve pg workList analysisResult tfRD
+//let b = workListAlgorithm <| pg <| [("Ø", 0, 0)] <| "dhat" <| tfRD
